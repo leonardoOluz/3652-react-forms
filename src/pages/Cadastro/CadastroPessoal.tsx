1c0ecbc34e1b84470140b18174a117bff7e0f913
Leonardo Luz <leonardoluz10@hotmail.com>
2024-10-21 21:51:55 -0300

    feat: :white_check_mark: Criando validações no formulario com formState errors.

--- src/pages/Cadastro/CadastroPessoal.tsx
+++ src/pages/Cadastro/CadastroPessoal.tsx
@@ -1,33 +1,53 @@
-import { useState } from "react";
-import { Button, Label, Fieldset, Input, Form, Titulo } from "../../components";
+import { useForm } from "react-hook-form";
+import { Button, Label, Fieldset, Input, Form, Titulo, ErrorMessage } from "../../components";
+
+interface FormCadastroProps {
+  nome: string;
+  email: string;
+  telefone: string;
+  senha: string;
+  senhaVerificada: string;
+}
+
 
 const CadastroPessoal = () => {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-  const [telefone, setTelefone] = useState("");
-  const [senha, setSenha] = useState("");
-  const [senhaVerificada, setSenhaVerificada] = useState("");
+  const { register, handleSubmit, formState: { errors } } = useForm<FormCadastroProps>()
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log({ nome, email, senha, telefone, senhaVerificada });
-  };
+  const onFormSubmit = (date: FormCadastroProps) => {
+    console.log(date)
+  }
+
+  const validateEmail = (value: string) => {
+    const validateRegex = /^\w+@alura\.com\.br$/
+    if (!validateRegex.test(value)) {
+      console.error("O email inserido não é válido")
+      return "O email inserido não é válido"
+    }
+
+    return true
+  }
 
   return (
     <>
       <Titulo>Insira alguns dados básicos:</Titulo>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit(onFormSubmit)}>
         <Fieldset>
           <Label htmlFor="campo-nome">Nome</Label>
           <Input
             id="campo-nome"
             placeholder="Digite seu nome completo"
             type="text"
-            value={nome}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setNome(e.target.value)
-            }
+            $error={!!errors.nome}
+            aria-describedby="error-message-nome"
+            {...register("nome", {
+              required: "O campo nome é obrigatório",
+              minLength: {
+                value: 3,
+                message: "O campo nome deve ter pelo menos 3 caracteres"
+              }
+            })}
           />
+          {errors.nome && <ErrorMessage id="error-message-nome">{errors.nome.message}</ErrorMessage>}
         </Fieldset>
         <Fieldset>
           <Label htmlFor="campo-email">E-mail</Label>
@@ -35,24 +55,26 @@ const CadastroPessoal = () => {
             id="campo-email"
             placeholder="Insira seu endereço de email"
             type="email"
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
+            $error={!!errors.email}
+            {...register("email", { required: "O campo email é obrigatório", validate: validateEmail })}
           />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </Fieldset>
-
         <Fieldset>
           <Label>Telefone</Label>
           <Input
             id="campo-telefone"
             type="text"
             placeholder="Ex: (DDD) XXXXX-XXXX"
-            value={telefone}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setTelefone(e.target.value)
-            }
+            $error={!!errors.telefone}
+            {...register("telefone", {
+              required: "O campo telefone é obrigatório", pattern: {
+                value: /^\(\d{2}\)\d{5}-\d{4}$/,
+                message: "O formato do telefone deve ser (XX)XXXX-XXXX"
+              }
+            })}
           />
+          {errors.telefone && <ErrorMessage>{errors.telefone.message}</ErrorMessage>}
         </Fieldset>
 
         <Fieldset>
@@ -61,11 +83,16 @@ const CadastroPessoal = () => {
             id="campo-senha"
             placeholder="Crie uma senha"
             type="password"
-            value={senha}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setSenha(e.target.value)
-            }
+            $error={!!errors.senha}
+            {...register("senha", {
+              required: "O campo senha é obrigatório",
+              minLength: {
+                value: 6,
+                message: "O campo senha deve ter pelo menos 6 caracteres"
+              }
+            })}
           />
+          {errors.senha && <ErrorMessage>{errors.senha.message}</ErrorMessage>}
         </Fieldset>
         <Fieldset>
           <Label htmlFor="campo-senha-confirmacao">Repita a senha</Label>
@@ -73,11 +100,12 @@ const CadastroPessoal = () => {
             id="campo-senha-confirmacao"
             placeholder="Repita a senha anterior"
             type="password"
-            value={senhaVerificada}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setSenhaVerificada(e.target.value)
-            }
+            $error={!!errors.senhaVerificada}
+            {...register("senhaVerificada", {
+              required: "O campo senha é obrigatório",
+            })}
           />
+          {errors.senhaVerificada && <ErrorMessage>{errors.senhaVerificada.message}</ErrorMessage>}
         </Fieldset>
         <Button type="submit">Avançar</Button>
       </Form>
